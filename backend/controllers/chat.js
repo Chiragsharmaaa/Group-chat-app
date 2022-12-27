b4673587a4477daaddbc067c361e1d2a1c6ba669
chiragsharmaaa <chiragsharma250999@gmail.com>
2022-12-28 02:21:15 +0530

    chat optimisation with local storage instead of setInterval.

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -7,7 +7,7 @@ exports.postMessage = async (req, res, next) => {
             return res.status(400).json({
                 message: "nothing entered!"
             });
-        }
+        };
         const data = await req.user.createChat({
             message
         });
@@ -19,22 +19,23 @@ exports.postMessage = async (req, res, next) => {
         res.status(500).json({
             message: "Unable to send messages!"
         });
-    }
+    };
 };
 
 exports.getMessage = async (req, res, next) => {
+    let msgId = req.query.lastmsgid;
     try {
         const data = await req.user.getChats();
+        let index = data.findIndex(chat => chat.id == msgId)
+        let msgtosend = data.slice(index + 1);
         let username = req.user.name;
         res.status(200).json({
-            data,
+            msgtosend,
             username
-        })
+        });
     } catch (error) {
         res.status(500).json({
             message: "Unable to retrieve chats!"
-        })
-    }
-
-
-}
+        });
+    };
+};
--- views/chat/chat.js
+++ views/chat/chat.js
@@ -1,38 +1,61 @@
 const token = localStorage.getItem("userToken");
+const User = localStorage.getItem('name');
 
 const chatList = document.getElementById('chat');
 const form = document.getElementById("chat-form");
 
 form.addEventListener("submit", chatFxn);
 
+let lastMsgId;
+let chatArr = [];
+
 window.addEventListener('DOMContentLoaded', screenLoader);
 
 async function screenLoader(e) {
     e.preventDefault();
+    const messages = JSON.parse(localStorage.getItem('msg'));
+    if (messages == undefined || messages.length == 0) {
+        lastMsgId = 0;
+    } else {
+        lastMsgId = messages[messages.length - 1].id;
+    };
 
     try {
-        const response = await axios.get('http://localhost:3000/user/getmessage', {
+        const response = await axios.get(`http://localhost:3000/user/getmessage?lastmsgid=${lastMsgId}`, {
             headers: {
                 "Authorization": token
             }
         });
-        showChatOnScreen(response.data, response.data.username);
+        console.log("Response", response)
+        var newArray = response.data.msgtosend;
+        saveToLocal(newArray, response.data.username);
     } catch (error) {
         console.log('Unable to retrieve messages!', error)
     };
+
 };
 
-function showChatOnScreen(data, name) {
+function saveToLocal(arr, name) {
+    let oldMessages = JSON.parse(localStorage.getItem('msg'));
+    if (oldMessages == undefined || oldMessages.length == 0) {
+        chatArr = chatArr.concat(arr);
+    } else {
+        chatArr = [];
+        chatArr = chatArr.concat(oldMessages, arr)
+    };
+    localStorage.setItem('msg', JSON.stringify(chatArr));
+    showChatOnScreen(name);
+}
+
+function showChatOnScreen(name) {
+    chatList.innerHTML = ''
     localStorage.setItem('name', name);
-    data.data.forEach(chat => {
-        showChats(chat, name);
-    });
-};
-
-function showChats(chat, name) {
-    let child = `<li class="me">
+    chatArr.forEach(chat => {
+        let child = `<li class="me" id=${chat.id}>
     <div class="entete">
-      <h3>${new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit', hour12:true}) + ' , ' + new Date().toDateString()}</h3>
+      <h3>${(new Date(Date.parse(chat.createdAt))).toLocaleString('en-US',
+      {timeZone:'UTC',hour12:true}
+    )}</h3>
       <h2>${name.split(' ')[0]}</h2>
       <span class="status blue"></span>
     </div>
@@ -42,7 +65,9 @@ function showChats(chat, name) {
     </div>
   </li>`;
 
-    chatList.innerHTML += child;
+        chatList.innerHTML += child;
+    });
+    document.getElementById(`${lastMsgId}`).scrollIntoView();
 }
 
 async function chatFxn(e) {
@@ -51,7 +76,6 @@ async function chatFxn(e) {
     const message = {
         message: e.target.message.value,
     };
-    console.log(message);
 
     try {
         const response = await axios.post(
@@ -63,26 +87,35 @@ async function chatFxn(e) {
             }
         );
         e.target.message.value = "";
-        showOnScreen(message);
+        let name = localStorage.getItem('name')
+        saveToLocal(response.data.data, name);
+        // showOnScreen(message);
     } catch (error) {
         console.log(error);
     };
 };
 
-function showOnScreen(chat) {
-    const name = localStorage.getItem('name');
+// function showOnScreen(chat) {
+//     const name = localStorage.getItem('name');
 
-    let child = `<li class="me">
-    <div class="entete">
-      <h3>${new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit', hour12:true}) + ' , ' + new Date().toDateString()}</h3>
-      <h2>${name.split(' ')[0]}</h2>
-      <span class="status blue"></span>
-    </div>
-    <div class="triangle"></div>
-    <div class="message">
-      ${chat.message}
-    </div>
-  </li>`
+//     let child = `<li class="me">
+//     <div class="entete">
+//       <h3>${new Date().toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit', hour12:true}) + ' , ' + new Date().toDateString()}</h3>
+//       <h2>${name.split(' ')[0]}</h2>
+//       <span class="status blue"></span>
+//     </div>
+//     <div class="triangle"></div>
+//     <div class="message">
+//       ${chat.message}
+//     </div>
+//   </li>`
 
-    chatList.innerHTML += child;
+//     chatList.innerHTML += child;
+// }
+
+document.getElementById('logout').onclick = function (e) {
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('msg');
+    localStorage.removeItem('name');
+    window.location.href = '../login/login.html';
 }
